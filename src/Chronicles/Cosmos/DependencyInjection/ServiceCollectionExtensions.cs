063ecc22e5d24776f47146e91370cdb1c77ca5fc
Ricky Kaare Engelharth <ricky@ricky.dk>
2023-08-03 11:43:20 +0200

    Initial thoughts on setup builder

--- src/Chronicles/Cosmos/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Chronicles/Cosmos/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,24 +1,22 @@
 using Chronicles.Cosmos;
 using Chronicles.Cosmos.Internal;
-using Chronicles.Cosmos.Serialization;
-using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection ConfigureChronicles(
-        this IServiceCollection services,
-        Func<IServiceProvider, ChroniclesCosmosOptions> optionsProvider)
-        => services
-            .AddSingleton(typeof(ICosmosReader<>), typeof(CosmosReader<>))
-            .AddSingleton(typeof(ICosmosWriter<>), typeof(CosmosWriter<>))
-            .AddSingleton<ICosmosContainerProvider, CosmosContainerProvider>()
-            .AddSingleton<IJsonCosmosSerializer, JsonCosmosSerializer>()
-            .AddSingleton<ICosmosClientProvider, CosmosClientProvider>()
-            .AddSingleton<ICosmosLinqQuery, CosmosLinqQuery>()
-            .AddSingleton<IOptions<ChroniclesCosmosOptions>>(s
-                => new OptionsWrapper<ChroniclesCosmosOptions>(optionsProvider(s)));
+    //public static IServiceCollection ConfigureChronicles(
+    //    this IServiceCollection services,
+    //    Func<IServiceProvider, ChroniclesCosmosOptions> optionsProvider)
+    //    => services
+    //        .AddSingleton(typeof(ICosmosReader<>), typeof(CosmosReader<>))
+    //        .AddSingleton(typeof(ICosmosWriter<>), typeof(CosmosWriter<>))
+    //        .AddSingleton<ICosmosContainerProvider, CosmosContainerProvider>()
+    //        .AddSingleton<IJsonCosmosSerializer, JsonCosmosSerializer>()
+    //        .AddSingleton<ICosmosClientProvider, CosmosClientProvider>()
+    //        .AddSingleton<ICosmosLinqQuery, CosmosLinqQuery>()
+    //        .AddSingleton<IOptions<ChroniclesCosmosOptions>>(s
+    //            => new OptionsWrapper<ChroniclesCosmosOptions>(optionsProvider(s)));
 
     public static IServiceCollection ConfigureChroniclesContainer<T>(
         this IServiceCollection services,
@@ -27,4 +25,34 @@ public static class ServiceCollectionExtensions
         => services
             .AddSingleton<ICosmosContainerNameProvider>(
                 s => new CosmosContainerNameProvider<T>(name));
+
+    public static IServiceCollection AddChronicles(
+        this IServiceCollection services,
+        Action<ChroniclesBuilder> builder)
+    {
+        builder.Invoke(new ChroniclesBuilder(services));
+        return services;
+    }
+
+    public static IServiceCollection AddChronicles(
+        this IServiceCollection services)
+        => AddChronicles(services, builder => { });
+}
+
+public class ChroniclesBuilder
+{
+    private readonly IServiceCollection services;
+
+    public ChroniclesBuilder(
+        IServiceCollection services)
+    {
+        this.services = services;
+    }
+
+    public ChroniclesBuilder WithOptions(
+        Action<ChroniclesCosmosOptions> optionsProvider)
+    {
+        services.Configure(optionsProvider);
+        return this;
+    }
 }
